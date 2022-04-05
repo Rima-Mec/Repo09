66b2a727b2d8da76d7846418a8d0b3b23b31454f
Rima-Mec <dmerima08@gmail.com>
2022-04-05 23:22:40 +0200

    Adding index.js file

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+// Only change code belowe this line
+function myTrueBoolean() {
+    var boolean = true;
+    boolean = true;
+    return true/false;
+}
+    // only change code above this line
+    
+    console.log((true));
+    module.exports = myTrueBoolean;
