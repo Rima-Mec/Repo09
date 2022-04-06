57d44af84d7912a196466856e29ce90263decdf3
Rima-Mec <dmerima08@gmail.com>
2022-04-06 04:32:45 +0200

    Adding index.js file

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+    Math.floor(Mathrandom() *(max - min +1)) + min; 
+    if(minNumber <= random) {
+    return random;
+    }
+    if(maxNumber >= random) {
+        return random;
+    }
+}
+    randomRangeNumber(20, 5);
+    console.log(randomRangeNumber(20, 5));
+ // Only change code above this number
+
+    module.exports = randomRangeNumber;
